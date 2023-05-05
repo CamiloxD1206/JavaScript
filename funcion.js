9ea4b0fbe88643e7acb588649dfece3b744bb9d6
Camilo <cetacue@misena.edu.co>
2023-05-05 17:57:36 -0500

    Cambios en la carpeta js

--- funcion.js
+++ funcion.js
@@ -24,19 +24,135 @@
 // semana.push(`sabado`);
 // console.table(semana);
 //crear un arreglo vacio que permita ingresar 3 motoviajeros con sus respectivas motos
-const objetos=[ {
-    nombre:prompt('¿cual es tu nombre?'),
-    motocicleta:prompt('¿cual es tu moto?'),
-    placa:prompt('cual es la placa de tu moto'),
-},
-{
-    nombre:prompt('¿cual es tu nombre?'),
-    motocicleta:prompt('¿cual es tu moto?'),
-    placa:prompt('cual es la placa de tu moto'),
-},
-{
-    nombre:prompt('¿cual es tu nombre?'),
-    motocicleta:prompt('¿cual es tu moto?'),
-    placa:prompt('cual es la placa de tu moto'),
-}]
-console.table(objetos);
+// const objetos=[ {
+//     nombre:prompt('¿cual es tu nombre?'),
+//     motocicleta:prompt('¿cual es tu moto?'),
+//     placa:prompt('cual es la placa de tu moto'),
+// },
+// {
+//     nombre:prompt('¿cual es tu nombre?'),
+//     motocicleta:prompt('¿cual es tu moto?'),
+//     placa:prompt('cual es la placa de tu moto'),
+// },
+// {
+//     nombre:prompt('¿cual es tu nombre?'),
+//     motocicleta:prompt('¿cual es tu moto?'),
+//     placa:prompt('cual es la placa de tu moto'),
+// },]
+// console.table(objetos);
+//---------------------------------------------------------------------------------
+//crear arreglo vacio,tres objetos (objeto 3 de primero,primero de 2,1 de tercero)
+// alert('Hola');
+// const motoviajero =[]
+
+// const objeto1={
+// nombre:prompt('tu nombre'),
+// placa:prompt('placa'),
+// moto:prompt('tu moto'),
+// }
+// const objeto2={
+//     nombre:prompt('tu nombre'),
+//     placa:prompt('placa'),
+//     moto:prompt('tu moto'),
+//     }
+//     const objeto3={
+//         nombre:prompt('tu nombre'),
+//         placa:prompt('placa'),
+//         moto:prompt('tu moto'),
+//         }
+
+//--------------------------------primera forma------------------------------------------------------
+// motoviajero[1]=objeto3;
+// motoviajero[2]=objeto2;
+// motoviajero[3]=objeto1;
+// console.table(motoviajero);
+
+//--------------------------------segunda forma------------------------------------------------------
+// motoviajero.unshift(objeto3);
+// motoviajero.push(objeto2);
+// motoviajero.push(objeto1);
+// console.table(motoviajero);
+//--------------------------------------------------------------------
+// alert('Hola');
+// const motoviajero =[]
+
+// const objeto1={
+// nombre:prompt('tu nombre'),
+// placa:prompt('placa'),
+// moto:prompt('tu moto'),
+// }
+// const objeto2={
+//     nombre:prompt('tu nombre'),
+//     placa:prompt('placa'),
+//     moto:prompt('tu moto'),
+//     }
+//     const objeto3={
+//         nombre:prompt('tu nombre'),
+//         placa:prompt('placa'),
+//         moto:prompt('tu moto'),
+//         }
+
+//         const union =[...motoviajero,objeto3 ];
+        
+//         const union =[...objeto3,objeto2 ];
+          
+//         const union =[...objeto1,motoviajero ];
+
+// console.table(union);
+//---------------------------------------------------seso-------------------
+// const hola=['Camilo','Cristian','Wilman','Daniel','Alvaro','Yecid','Aldair','Luis'];
+// const [ , aprendiz1, , , aprendiz4, , , aprendiz8] = hola;
+// console.log(aprendiz4); 
+//--------------------------------for-(estudialo cabron porfavor)---------------------------------------
+// alert('Hola');
+// const Hola =[]
+//     for(let i=0;i<10;i++){
+//         motoviajero1={
+//             nombre:prompt('tu nombre'),
+//             placa:prompt('placa'),
+//             moto:prompt('tu moto'),
+//             };
+//             Hola.unshift(motoviajero1);
+//            }
+//         console.table(Hola);
+        
+        
+//----------------------------------------------------------
+// const Hola =[motoviajero1={
+//     nombre:prompt('tu nombre'),
+//     placa:prompt('placa'),
+//     moto:prompt('tu moto'), }]
+
+//             Hola.forEach(function(datos) { 
+//                 console.table(`Nombre:${datos.nombre} Placa:${datos.placa} Moto: ${datos.moto} `);});
+         
+
+//for2---------------------------------------------
+// const motoviajero =[{
+//     nombre:prompt('tu nombre'),
+//     placa:prompt('placa'),
+//     moto:prompt('tu moto'),
+//     },
+//     {
+//         nombre:prompt('tu nombre'),
+//         placa:prompt('placa'),
+//         moto:prompt('tu moto'),
+//         },
+//         {
+//             nombre:prompt('tu nombre'),
+//             placa:prompt('placa'),
+//             moto:prompt('tu moto'),
+//             },];
+
+      
+                
+//             motoviajero.forEach(function(datos) {
+//                 console.table(`Nombre:${datos.nombre} Placa:${datos.placa} Moto: ${datos.moto} `);
+//             });
+//funciones-----------------------------------------
+
+    function multiplicar(){
+        console.log(1+2);
+    }      
+multiplicar();
+  
